df139b4018a6fbf463a5b96b716ffee9bf04f3e1
unknown <rjmahir.faisal@gmail.com>
2022-02-06 21:23:13 +0600

    scroll bar style added

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+
+
+
+
   return (
     <div className={styles.container}>
       <Head>
