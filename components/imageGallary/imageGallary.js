fa8f1e5c55c9dc4963cb24adc4568623889b877f
ekholil <kholilebrahim01@gmail.com>
2022-02-07 18:38:54 +0600

    image gallery updated

--- components/imageGallary/imageGallary.js
+++ components/imageGallary/imageGallary.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 import { FaRegImages } from "react-icons/fa";
 const ImageGallary = ({ data }) => {
   return (
-    <div className="mx-32">
+    <div className="md:mx-32">
       <h2 className="text-lg py-y">Photo Gallary</h2>
       {console.log(data)}
-      <div className="grid grid-cols-4 gap-3 ">
-        <div className="relative w-full col-span-2 row-span-2 img-overlay">
+      <div className="grid md:grid-cols-4 gap-3 sm:grid-cols-1 ">
+        <div className="relative w-full md:col-span-2 md:row-span-2 sm:col-span-1 sm:row-span-1 img-overlay">
           <div className="absolute z-30 bg-slate-200 rounded-full m-2 p-2">
             <FaRegImages className="text-2xl" />
           </div>
@@ -19,7 +19,7 @@ const ImageGallary = ({ data }) => {
           ></Image>
           <h1 className="absolute bottom-3 left-3 text-white text-2xl">{data[0].name}</h1>
         </div>
-        <div className="relative w-full col-span-2">
+        <div className="relative w-full md:col-span-2 sm:col-span-1">
         <div className="absolute z-30 bg-slate-200 rounded-full m-2 p-2">
             <FaRegImages className="text-2xl" />
           </div>
