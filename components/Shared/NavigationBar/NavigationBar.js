72ee8f6a021a51d1679bea80679b2f2eb81fdc56
emranhossain-wdd <emranhossain4005@gmail.com>
2022-02-08 10:17:55 +0600

    pulled

---
+++ components/Shared/NavigationBar/NavigationBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavigationBar = () => {
+    return (
+        <div>
+            Hi
+        </div>
+    );
+};
+
+export default NavigationBar;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import NavigationBar from "../components/Shared/NavigationBar/NavigationBar";
+
 export default function Home() {
 
 
@@ -7,6 +9,7 @@ export default function Home() {
   return (
     <div>
       <h1 className='text-5xl text-yellow-500'> Hello Team mates, Start work from here </h1>
+      <NavigationBar />
     </div>
   )
 }
