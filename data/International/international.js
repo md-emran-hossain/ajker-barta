9e52e735985c542760e71a49f5a1423da8f7f5e6
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-02-09 00:55:40 +0600

    international sections Asia news data added

---
+++ data/International/international.js
@@ -0,0 +1,97 @@
+const international = [
+    {
+        id:1,
+        country:'Asia',
+        news:[
+            {
+                id:1, 
+                title: 'Hong Kong activist arrested ahead of planned Olympics protest',
+                img: 'https://images.prothomalo.com/prothomalo-english%2Fimport%2Fmedia%2F2019%2F08%2F16%2F5659c2d81cc412cd5a7cf4c645483580-Hong-Kong-4.jpg?auto=format%2Ccompress&fmt=webp&format=webp&w=940&dpr=1.0',
+                description: 'A veteran Hong Kong activist was arrested for “incitement to subversion” on Friday ahead of a planned protest against Beijings hosting of the Winter Olympics, hours before the opening ceremony was due to kick off.China has embarked on a crackdown in Hong Kong following massive and at times violent pro-democracy protests, imposing a sweeping national security law that criminalised much dissent.AdvertisementWen Wei Po, a newspaper that answers to Beijing’s Liaison Office in Hong Kong, said Koo Sze-yiu was arrested on Friday morning by national security police.A senior police source confirmed to AFP that Koo had been detained for “incitement to subversion” and that four others had been taken in to assist with their investigation.The arrest comes days after Hong Kong journalists received a media invitation bearing Koo’s name saying he was planning to protest on Friday outside Beijing’s Liaison Office in Hong Kong ahead of the start of the Olympics.“Many political activists and Hong Kong citizens have been jailed and cannot spend Lunar New Year with their families because of abuses of Hong Kong’s national security law,” read the invitation, dated 31 January.',
+                date: '14 July 2019',
+                
+            },
+            {
+                id: 2,
+                title: 'Myanmar shadow government drops objections to ICJs Rohingya genocide case',
+                img:'https://images.prothomalo.com/prothomalo-english/2022-02/f34dd3c0-f7dc-420d-b6a8-62d8c9dd8970/2022_02_02T040848Z_1999362388_RC2GBS9R5XHY_RTRMADP_3_MYANMAR_ROHINGYA_WORLD_COURT.JPG?format=webp&w=1440&dpr=1.0',
+                description:'Myanmars shadow government, set up after last years military coup, said it accepts the jurisdiction of the International Court of Justice (ICJ) to hear allegations that the country committed genocide against its Rohingya minority.Before the military seized power last year, Myanmars government led by the now-ousted Aung San Suu Kyi had filed preliminary objections to the ICJ over the case brought by Gambia in a move seen as likely to delay proceedings.The National Unity Government (NUG), a parallel administration including deposed lawmakers in exile, said in a statement issued on Tuesday that it had withdrawn all preliminary objections to the case.Advertisement Still, it is not clear whether this would affect the legal process since the NUG said that through a "bureaucratic idiosyncrasy" the ICJ has been communicating with Myanmar diplomats in Brussels who were under the control of the junta.',
+                date: '27 September 2017'
+            },
+            {
+                id: 3,
+                title:'UN, rights groups blame Taliban for missing journalists',
+                img:'https://images.prothomalo.com/prothomalo-english%2F2021-11%2F2b609139-9790-435a-9a29-b6ee6cd04b7e%2F210905_01_02.jpg?rect=0%2C0%2C3873%2C2179&auto=format%2Ccompress&fmt=webp&format=webp&w=940&dpr=1.0',
+                description:'The Taliban have arrested two Afghan journalists working for a local news channel, rights groups and the United Nations said Tuesday, weeks after two women activists went missing.Since seizing power in August, the hardline Islamists have cracked down on dissent by detaining critics and forcefully dispersing protests against their regime.Several Afghan journalists have also been beaten while covering rallies not approved by authorities.Advertisement The Afghan Media Association—a newly formed journalists’ rights group—said Ariana TV reporters Waris Hasrat and Aslam Hijab were picked up by the Taliban on Monday “and taken to an unknown location”.Without naming the Taliban, an official at Ariana told AFP the reporters were seized by masked gunmen in front of the channel’s office as they went out for lunch.',
+                on_date:'15 November 2021',
+                publish_data: '02 Feb 2022'
+            },
+            {
+                id: 4,
+                title:'Erdogan seeks payoff from Russia-US clash on Ukraine',
+                img:'',
+                description:'Turkish president Recep Tayyip Erdogan will try to leverage his strategic position in NATO and his rapport with Russia’s Vladimir Putin when he visits Kyiv on Thursday in a bid to head off war in Ukraine.The veteran Turkish leader hopes mediation between Putin and Ukrainian President Volodymyr Zelensky can avert a Russian offensive that Washington warns could start by mid-February.His high-profile efforts—met with caution in Moscow—carry huge stakes and potentially rich rewards.Analysts believe a serious conflict in Ukraine could upend Turkey’s economy and imperil Erdogan’s chances of extending his rule into a third decade in elections due by mid-2023.Advertisement It could also force Ankara to pick sides between Putin—a leader who holds several economic and military trump cards over Turkey—and traditional Western allies that have grown impatient with Erdogan’s rule.Kyiv’s acquisition of battle-tested Turkish drones is a particular worry for Russian-backed separatist fighters in eastern Ukraine and for the Kremlin.',
+                on_date:'21 August 2020',
+                publish_date: '02 Feb 2022'
+            },
+            {
+                id: 5,
+                title:'Myanmars Suu Kyi: Prisoner of generals',
+                img:'https://images.prothomalo.com/prothomalo-english%2F2021-09%2F46bcdf44-3255-4fd9-b4ab-9c210f795f0a%2FMYANMAR_POLITICS.JPG?auto=format%2Ccompress&fmt=webp&format=webp&w=940&dpr=1.0',
+                description:'Myanmars latest democracy struggle enters its second year with its best-known figurehead, Aung San Suu Kyi, isolated from the fight, a prisoner of the generals behind the coup and facing the rest of her life in prison.The Nobel laureate has been held since the putsch that ousted her government in the early hours of 1 February last year, ending a brief democratic interlude for the country and sparking huge protests.Months before, her National League for Democracy (NLD) had swept nationwide elections and she had been preparing to begin another five-year term as the nations de facto leader.The daughter of an independence hero, Suu Kyi spent nearly two decades enduring long stretches of house arrest under the former military regime.Advertisement She has been jailed by the current junta for six years after being found guilty of illegally importing and possessing walkie-talkies, flouting coronavirus rules and incitement against the military.',
+                on_date:'9 October 2018',
+                publish_date:'02 Feb 2022'
+            },
+            {
+                id: 6,
+                title:'Japan finds piece of missing F-15, searching for crew',
+                img:'https://images.prothomalo.com/prothomalo-english%2Fimport%2Fmedia%2F2014%2F09%2F06%2F3f8b454aa016988e055da7e4394b0248-_77410226_77406464.jpg?auto=format%2Ccompress&fmt=webp&format=webp&w=940&dpr=1.0',
+                description:'Japanese aircraft and boats were searching Tuesday for two crew from a fighter jet that is believed to have crashed after taking off a day earlier, a government spokesman said.Part of the F-15 jet has been found, but authorities are still searching for the two people on board, top government spokesman Hirokazu Matsuno told reporters.The jet, which was on a training mission, disappeared from radar shortly after it took off late Monday afternoon, Matsuno said.It disappeared about five kilometres (three miles) west-northwest of the Komatsu airbase in central Ishikawa region, off the Sea of Japan.Advertisement"As part of the fuselage of the fighter jet was discovered in the area, the fighter jet is believed to have crashed," Matsuno said.The spokesman said "all efforts" were being made to locate the missing crew members, with Self-Defense Force aircraft and vessels as well as coastguard patrol boats searching the area.No explanation for the crash has been given so far, and the defence ministry has not sought to ground F-15s in the wake of the incident.Japan has occasionally seen accidents involving its airforce, including in 2019 when an F-35A stealth jet crashed into the sea after the pilot suffered spatial disorientation.The crash sparked a scramble to recover the pilot and secrets onboard the plane.',
+                on_date:'01 Feb 2022',
+                publish_date:'01 Feb 2022',
+            },
+            {
+                id: 7,
+                title:'',
+                img:'',
+                description:'',
+                date:''
+            },
+            {
+                id: 8,
+                title:'',
+                img:'',
+                description:'',
+                date:''
+            },
+            {
+                id: 9,
+                title:'',
+                img:'',
+                description:'',
+                date:''
+            },
+            {
+                id: 10,
+                title:'',
+                img:'',
+                description:'',
+                date:''
+            },
+            {
+                id: 11,
+                title:'',
+                img:'',
+                description:'',
+                date:''
+            },
+            {
+                id: 12,
+                title:'',
+                img:'',
+                description:'',
+                date:''
+            }
+        ]
+    }
+]
