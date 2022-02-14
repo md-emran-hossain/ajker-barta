63c50e980d2288fc5b114aadd9a124602a17b4cd
emranhossain-wdd <emranhossain4005@gmail.com>
2022-02-15 00:09:01 +0600

    api calling changed

--- pages/[category]/[subcategory].js
+++ pages/[category]/[subcategory].js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Footer from '../../components/Shared/Footer/Footer';
 import Header from '../../components/Shared/Header/Header';
 import NavigationBar from '../../components/Shared/NavigationBar/NavigationBar';
@@ -15,16 +15,13 @@ const SubCategoryDetails = ({ newses }) => {
   const unique = [...new Set(subCategories)];
   const displayNews = newses.filter(news => news.subCategory === subCategory)
 
-  console.log(subCategory, category)
-  
-  
   return (
     <div>
-        <Header />
-        <NavigationBar />
-        
+      <Header />
+      <NavigationBar />
+
       <div className={styles.smallContainer}>
-      <div className='flex gap-2 items-center'>
+        <div className='flex gap-2 items-center'>
           <h1 className='text-3xl font-bold'>{category}</h1>
           <p>{subCategory}</p>
         </div>
@@ -38,8 +35,8 @@ const SubCategoryDetails = ({ newses }) => {
             displayNews?.slice(0, 5).map(news => <div className={styles.itemBox} key={news.id}>
               <img src={news?.images?.img1} alt="" />
               <h1>{news?.heading}</h1>
-              <p>{news.description?.[0]}</p>
-              <p>{news.publishedDate}</p>
+              <p>{news?.description?.[0]}</p>
+              <p>{news?.publishedDate}</p>
             </div>)
           }
         </div>
@@ -50,7 +47,7 @@ const SubCategoryDetails = ({ newses }) => {
               <div>
                 <h1 className='text-xl font-medium hover:text-red-600 transition-colors duration-300 cursor-pointer'>{news?.heading}</h1>
                 <p className='text-sm my-2'>{news?.description?.[0]}</p>
-                <p className='text-blue-600 text-md'>{news.date}</p>
+                <p className='text-blue-600 text-md'>{news?.date}</p>
               </div>
             </div>)
           }
@@ -64,10 +61,10 @@ const SubCategoryDetails = ({ newses }) => {
 
 export default SubCategoryDetails;
 export const getServerSideProps = async () => {
-    const res = await axios.get(`http://localhost:3000/api/news/`);
-    return {
-      props: {
-        newses: res.data,
-      },
-    };
+  const res = await axios.get(`https://ajker-barta.vercel.app/api/news/`);
+  return {
+    props: {
+      newses: res.data,
+    },
   };
+};
