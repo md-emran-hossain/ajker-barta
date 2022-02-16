0274bae467a91d774de81e3749103208541d1813
Khayrul Islam <khayrulislam2525@gmail.com>
2022-02-16 18:47:15 +0600

    user and admin adding function added

--- hooks/useFirebase.js
+++ hooks/useFirebase.js
@@ -207,41 +207,32 @@ export default function useFirebase() {
 
 
     // admin set to database 
-    // useEffect(() => {
-    //     fetch(`/users/${user.email}`)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             setAdmin(data.admin)
-    //             console.log(data.admin)
-    //         })
-    //         .catch(error => {
-    //             Swal.fire({
-    //                 icon: 'error',
-    //                 title: 'Oops...',
-    //                 text: `${error} `,
+    useEffect(() => {
+        fetch(`https://ajker-barta.vercel.app/api/users/${user.email}`)
+            .then(res => res.json())
+            .then(data => {
+                setAdmin(data.admin)
+            })
+    }, [user.email]);
 
-    //             })
-    //         })
-    // }, [user.email]);
-
-    /// user info save to the database 
-    // const saveUser = (email, displayName, method) => {
-    //     const user = { email, displayName };
-    //     console.log(user);
-    //     fetch('', {
-    //         method: method,
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify(user)
-    //     })
-    //         .then(res => {
-    //             setLoading(false)
-    //         })
-    //         .catch(error => {
-    //             console.log(error);
-    //         })
-    // };
+    // user info save to the database 
+    const saveUser = (email, name, method) => {
+        const user = { name, email };
+        console.log(user);
+        fetch('https://ajker-barta.vercel.app/api/users', {
+            method: method,
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => {
+                setLoading(false)
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    };
 
     return {
         user, admin, authError, loading, signInWithGoogle, registerUser, loginUser, logOut, setLoading, setAuthError
