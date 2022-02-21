8e02231bee480475e6778c92b76037ff1bbb1b4f
unknown <rjmahir.faisal@gmail.com>
2022-02-21 15:18:50 +0600

    make admin api chenged

--- components/Dashboard/MakeAdmin/MakeAdmin.js
+++ components/Dashboard/MakeAdmin/MakeAdmin.js
@@ -27,7 +27,7 @@ const MakeAdmin = () => {
             reverseButtons: true
         }).then((result) => {
             if (result.isConfirmed) {
-                fetch('/users/admin', {
+                fetch('/api/users', {
                     method: 'PUT',
                     headers: {
                         'content-type': 'application/json'
