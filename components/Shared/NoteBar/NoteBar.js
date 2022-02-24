de3a3a19a4a3b711990cd3bd5eaf82eb0b301ccc
Khayrul Islam <khayrulislam2525@gmail.com>
2022-02-24 22:54:41 +0600

    add note system added

--- components/Shared/NoteBar/NoteBar.js
+++ components/Shared/NoteBar/NoteBar.js
@@ -1,26 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useAuth from '../../../hooks/useAuth';
+import Swal from 'sweetalert2'
 
 const NoteBar = ({ isShowNoteBar, setIsShowNoteBar, selectedText }) => {
   const { user } = useAuth()
   const [note, setNote] = useState('')
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    fetch(`/api/users/note?email=${user.email}`)
+      .then(res => res.json())
+      .then(data => setNotes(data.notes || []))
+  }, [user.email])
+
+
   const saveNote = () => {
-    const newObj = { note, selectedText, email: user.email }
-    fetch('/api/users/note', {
+    const allNotes = [...notes, { note, selectedText }]
+    const noteObj = { allNotes, email: user.email }
+    fetch(`/api/users/note`, {
       method: "PUT",
       headers: {
         'content-type': "application/json"
       },
-      body: JSON.stringify(newObj)
+      body: JSON.stringify(noteObj)
     })
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        if (data.modifiedCount === 1) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: `Your Note added successfully`,
+          })
+          setIsShowNoteBar(false)
+          setNote('')
+        }
+      })
   }
   return (
     <div className={isShowNoteBar ? 'w-96 bg-white fixed h-screen top-0 right-0 z-50 py-16 px-3 transition-right duration-300' : 'w-96 bg-white fixed h-screen top-0 -right-96 z-50 py-16 px-3 transition-right duration-300'}>
       <h1>{`${user.displayName}'s Note`}</h1>
       <textarea value={selectedText || ''} rows="10" className='w-full outline-0 bg-red-100 my-2 p-2'></textarea>
-      <textarea onBlur={(e) => setNote(e.target.value)} type="text" className='w-full py-1 px-2 bg-red-200 outline-0 mb-2' />
+      <textarea placeholder='Title' value={note || ''} onChange={(e) => setNote(e.target.value)} type="text" className='w-full py-1 px-2 bg-red-200 outline-0 mb-2' />
       <button onClick={saveNote} className='py-1 px-3 bg-blue-500 rounded-full text-white'>Add Note</button>
       <button onClick={() => setIsShowNoteBar(false)} className='py-1 px-3 bg-red-500 rounded-full text-white ml-2'>Cencel</button>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -8,10 +8,6 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
-import { useState } from 'react';
-import { MdOutlineEditNote, MdFacebook } from 'react-icons/md'
-import { AiOutlineTwitter } from 'react-icons/ai'
-import NoteBar from '../components/Shared/NoteBar/NoteBar';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -19,53 +15,11 @@ const clientSideEmotionCache = createEmotionCache();
 function MyApp({ Component, pageProps }) {
   // const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   const emotionCache = clientSideEmotionCache
-  const [selectedText, setSelectedText] = useState('')
-  const [showTextOption, setShowTextOption] = useState(false)
-  const [xValue, setxVlue] = useState(0)
-  const [yValue, setyVlue] = useState(0)
-  const [isShowNoteBar, setIsShowNoteBar] = useState(false)
-  const [isHighLight, setIsHighLight] = useState(false)
 
-  //handle selection
-  const handleSelection = (e) => {
-    setTimeout(() => {
-      const text = window.getSelection().toString().trim();
-      if (text.length) {
-        setSelectedText(text)
-        setShowTextOption(true)
-        const x = e.pageX;
-        const y = e.pageY;
-        setxVlue(x)
-        setyVlue(y)
-      } else {
-        setShowTextOption(false)
-      }
-    }, 0)
-  };
-  const addToNote = (e) => {
-    e.preventDefault()
-    setIsShowNoteBar(true)
-    setIsHighLight(!isHighLight)
-  }
-
-  //twitter share
-  const shareOnTwitter = () => {
-    const twitterShareUrl = "https://twitter.com/intent/tweet";
-    const text = `${encodeURIComponent(selectedText)}`;
-    const currentUrl = encodeURIComponent(window.location.href);
-    const hashtags = "HotNews, Recent, Letest";
-    const via = "Ajker Barta";
-    window.open(`${twitterShareUrl}?text="${text}"&url=${currentUrl}&hashtags=${hashtags}&via=${via}`);
-  }
-
-  //facebook share
-  const faceBookShare = () => {
-    window.open(`https://www.facebook.com/sharer.php?`)
-  }
 
   return (
 
-    <div onMouseUp={handleSelection} >
+    <div >
       <CacheProvider value={emotionCache}>
         <Head>
           <title>
@@ -82,17 +36,12 @@ function MyApp({ Component, pageProps }) {
             <CssBaseline />
             <AuthProvider>
               <Component {...pageProps} />
-              <NoteBar isShowNoteBar={isShowNoteBar} setIsShowNoteBar={setIsShowNoteBar} selectedText={selectedText} />
             </AuthProvider>
           </ThemeProvider>
         </LocalizationProvider>
 
       </CacheProvider>
-      <div style={{ left: (xValue - 70) + 'px', top: (yValue - 60) + 'px', }} className={showTextOption ? 'afterSelectBtn showOption' : 'afterSelectBtn'}>
-        <button onClick={addToNote} ><MdOutlineEditNote /></button>
-        <button onClick={shareOnTwitter}><AiOutlineTwitter /></button>
-        <button onClick={faceBookShare}><MdFacebook /></button>
-      </div>
+
     </div >
   )
 };
