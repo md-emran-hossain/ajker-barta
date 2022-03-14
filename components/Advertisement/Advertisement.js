fb59b88a2f76c44d86daae5692c7c199cec59ba2
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-03-15 00:39:17 +0600

    design edit

--- components/Advertisement/Advertisement.js
+++ components/Advertisement/Advertisement.js
@@ -5,15 +5,21 @@ const Advertisement = () => {
     return (
         <>
             <div className='container'>
-                <div className='className="text-justify shadow-2xl px-8 py-8"'>
-                    <h2 className='text-2xl font-medium mt-5 mb-2 text-justify text-slate-500 decoration-zinc-900 hover:text-blue-400'>Advertisement</h2>
+                <div className='className="text-justify shadow-2xl px-8 py-8 mx-4"'>
+                    <h2 className='text-3xl font-bold px-6 mt-5 mb-2 text-justify text-slate-500 decoration-zinc-900 hover:text-blue-400'>Advertisement</h2>
                     <hr />
                     <br />
-                    <p>As per the latest National Media Survey (NMS 2018 of Kantar MRB), 6.6 million people read the print edition of Ajker Barta every day, and it is the highest readership number among all Bangladeshi newspapers. Ajker Barta does not only have the widest reader base as a whole, but it also reaches to the most erudite readers of Bangladesh belonging to the upper SEC (Socio Economic Class). Ajker Barta is way ahead of any other newspaper in reaching key target groups of readers.</p>
+                    <p className='text-justify mx-10'>As per the latest National Media Survey (NMS 2018 of Kantar MRB), 6.6 million people read the print edition of Ajker Barta every day, and it is the highest readership number among all Bangladeshi newspapers. Ajker Barta does not only have the widest reader base as a whole, but it also reaches to the most erudite readers of Bangladesh belonging to the upper SEC (Socio Economic Class). Ajker Barta is way ahead of any other newspaper in reaching key target groups of readers.</p>
                     <br />
-                    <p>Ajker Barta is the “Number 1” Bangladeshi as well as Bengali Language website in the world based on traffic and pageviews. Around 12 million users access this portal every month from over 200 different countries and territories. They generate around 280 million pageviews per month. Through all digital platforms, Bengali and English portals and mobile apps, Ajker Barta Online reaches over 1 million people every day at home and abroad.</p>
+                    <p className='text-justify mx-10'>Ajker Barta is the “Number 1” Bangladeshi as well as Bengali Language website in the world based on traffic and pageviews. Around 12 million users access this portal every month from over 200 different countries and territories. They generate around 280 million pageviews per month. Through all digital platforms, Bengali and English portals and mobile apps, Ajker Barta Online reaches over 1 million people every day at home and abroad.</p>
                 </div>
+
                 {/* print addition */}
+
+                <div>
+
+                </div>
+
             </div>
         </>
     );
