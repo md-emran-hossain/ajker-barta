a4101f797a2ab4dabf9776d865136b8597d6429c
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-03-14 23:32:35 +0600

    making advertisement page

---
+++ components/Advertisement/Advertisement.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Advertisement = () => {
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    );
+};
+
+export default Advertisement;
