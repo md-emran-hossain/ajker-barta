9e01c4ff79d67087bc46edb14877d41f7eaba0d8
unknown <sanjuhassan916@gmail.comgit config --global user.name Your>
2022-03-15 10:17:10 +0600

    Careeritem

--- components/Career/Careeritem.js
+++ components/Career/Careeritem.js
@@ -2,108 +2,83 @@ import React from 'react';
 
 const Careeritem = () => {
     return (
-        <div>
-            
-
-
-      <div className="cpbody">
-          <div className="positiontest">
-            
-                <div className="containimage">
-                    <div className="slidecontainer">
-                        <div id="slideshow">
-                          <img className="imageflex" src="https://res.cloudinary.com/huda-tariq-design/image/upload/v1484581124/iStock-test_i4rt8j.jpg"/> 
-                          
-                        </div>
-                    </div>
-                        <div className="overlay">
-                        <h1 className="esb">EMPOWERING SMALL BUSINESSES</h1>
-                        </div>
-                  </div>
+       <>
+    
+       
+  <div className=" container py-20 bg-cover bg-no-repeat" style={{backgroundImage: `url(https://w7.pngwing.com/pngs/562/604/png-transparent-madison-area-technical-college-finding-the-right-career-path-career-pathways-career-development-academics-text-people-logo-thumbnail.png)`}}>
+    <div className="container m-auto text-center px-6 opacity-100">
+      <h2 className="text-4xl font-bold mb-2 text-red">Career !</h2>
+    
+      <button className="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider hover:border-transparent hover:text-blue-500 hover:bg-gray-800">On its career do you copy?</button>
+    </div>
+  </div>
+        
+        
+        <section className="container mx-auto px-6 p-10">
+          <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">My Carrier Update!</h2>
+          <div className="flex items-center flex-wrap mb-20">
+            <div className="w-full md:w-1/2 pr-10">
+              <h4 className="text-3xl text-gray-800 font-bold mb-3">Who We Are</h4>
+              <p className="text-gray-600 mb-8">Your future is waiting for you. What will you choose to become? How do you want to live your life? What career is right for you? This free career test is for students and adults to help you discover your career interests because there are thousands of careers to choose from and each is just as unique as you! Give us just</p>
+            </div>
+            <div className="w-full md:w-1/2">
+              <img className="rounded-lg" src="https://pbs.twimg.com/media/CR45LOXVEAADG5E.jpg" alt="Vortex" />
+            </div>
           </div>
-      </div> 
-
-
+          <div className="flex items-center flex-wrap mb-20">
+            <div className="w-full md:w-1/2">
+              <img className="rounded-lg" src="https://www.thesun.co.uk/wp-content/uploads/2019/06/SWJFO-EAPlay-08-1.jpg" alt="use the force" />
+            </div>
+            <div className="w-full md:w-1/2 pl-10">
+              <h4 className="text-3xl text-gray-800 font-bold mb-3">Our Mission!</h4>
+              <p className="text-gray-600 mb-8"> You can think of a mission statement as a combination of what your business or nonprofit does and how and why it does it, expressed in a way that encapsulates the values that are important to you. It can be a challenge to clearly and concisely bring these ideas together, though. Here is a simple guide—along with some examples—for writing your own company mission statement!</p>
+            </div>
+          </div>
+          <div className="flex items-center flex-wrap mb-20">
+            <div className="w-full md:w-1/2 pr-10">
+              <h4 className="text-3xl text-gray-800 font-bold mb-3">Our Culture</h4>
+              <p className="text-gray-600 mb-8">The culture in its simplest form refers to the ideas, customs, and social behaviour of particular people or society. Moreover, it denotes the arts, events, actions and objects that clearly show or represent something abstract or theoretical of human intellectual achievement regarded collectively. We also relate culture to the attitudes and behaviour characteristic of a specific social</p>
+            </div>
+            <div className="w-full md:w-1/2">
+              <img className="rounded-lg" src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Ferikkain%2Ffiles%2F2018%2F01%2FRey-Luke.jpg" alt="Syncing" />
+            </div>
+          </div>
+        </section>
+        
+       
+        <section className="bg-gray-100">
+          <div className="container mx-auto px-6 py-20">
+            <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">Career expresse</h2>
+            <div className="flex flex-wrap">
+              
+              <div className="w-full h-auto md:w-1/3 px-2 mb-4">
+                <div className="flex flex-col justify-between h-full bg-white rounded shadow py-2">
+                  <p className="text-gray-800 text-base px-6 mb-5">Equity, Diversity and Inclusion are embedded in our DNA and fundamental to our success. We believe that innovation, technology and inclusion can help build a bette</p>
+                
+                </div>
+              </div>
+              
+              <div className="w-full h-auto md:w-1/3 px-2 mb-4">
+                <div className="flex flex-col justify-between h-full bg-white rounded shadow py-2">
+                  <p className="text-gray-800 text-base px-6 mb-5">world and boast employees across cultures and countries. More than a program for us, Corporate Citizenship is engrained in our purpose, </p>
+                  
+                </div>
+              </div>
+              
+              <div className="w-full h-auto md:w-1/3 px-2 mb-4">
+                <div className="flex flex-col justify-between h-full bg-white rounded shadow py-2">
+                  <p className="text-gray-800 text-base px-6 mb-5">Our growing team is mapping the future of Information Management, and we continue to make new discoveries that expand its boundaries. With our breadth of products,</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        
+   
+        
+      
      
-        <div className="careercontent">
-        <h2 className="sh">Our Mission</h2>  
-        <p>SinaLite was founded to Empower small business owners  in the advertising and marketing sector to boost their sales and profitability. We strive to be a leader by offering high-quality marketing materials through ecommerce and lean manufacturing. 
-        </p>
-        </div>
-
-        <div className="careercontent">
-        <h2 className="sh">Who We Are</h2>  
-        <p>SinaLite offers flexible marketing solutions for business owners in the advertising and marketing sector within North America. With our state-of-the-art manufacturing facility and our online technology, we produce and fulfill over five million orders each year. 
-        </p>
-        <p>
-        In order to provide our clients with innovative solutions, we embrace new developments in e-commerce, manufacturing technology and marketing trends. And everyday, our talented team keeps SinaLite moving towards our company mission.</p>
-        </div>
-        
-
-
-
-          <div>
-              <div className="testcontainer">
-              <div className="textpostion">
-              <img className="flextwo" src="https://res.cloudinary.com/huda-tariq-design/image/upload/v1484581124/iStock-test_i4rt8j.jpg"/>
-              <div className="blfovrly overlaytwo">
-              <h2 className="blfo">Beliefs</h2> 
-             <p>Everyday at SinaLite, our core beliefs guide us towards achieving our mission as a team.
-              </p>
-              </div>
-              </div>
-              </div>
-          </div>
-
-        <div className="beliefcon">
-        <div className="bdiv">
-            <div className="bcards">
-             
-             <h3 className="hcard">Focus on Customers</h3> 
-              <p>  
-              We care about our customers. Helping small businesses grow is top of mind when we provide innovative, swift solutions for our customers. 
-              </p>
-            </div>
-            <div className="bcards">
-              
-              <h3 className="hcard">Improve the Process</h3> 
-              <p>  
-              We look for the best ways to solve a problem. By focusing on how we can do something better, we can complete projects more easily and efficiently.
-              </p> 
-            </div>
-            <div className="bcards"> 
-              
-              <h3 className="hcard">Take the Risk</h3> 
-              <p>
-              Being bold and taking risks by pursuing new ideas
-              </p>
-            </div>
-            <div className="bcards">
-              
-              <h3 className="hcard">Enjoy the Work</h3>
-              <p>
-              We love what we do. 
-              being professionals at what we do, and having fun doing it.
-              </p>
-            </div>
-            </div>
-        </div>
-        <div className="careercontent">
-        <h2 className="sh">Our Culture</h2>
-        <p>It is our team who makes SinaLite. We come from diverse backgrounds, but we share the same common goal: to empower small business owners in the advertising and marketing sector to boost their sales and profitability.
-        </p>
-        <p>  
-        SinaLite encourages an open culture, where every team member is a hands-on contributor. We are proud to maintain an environment in which employees feel comfortable sharing ideas and suggestions.
-        </p>
-        <p>
-        Our team is made up of ambitious and talented individuals who are crucial to our continued success. While experience is an advantage for our candidates, we favour individuals who are capable and driven.
-        </p>
-
-        </div>
-
-        
-            
-       </div>
+       </>
        
         
     );
