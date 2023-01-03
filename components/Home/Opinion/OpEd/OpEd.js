8c0736ecf508b2c869d263a26aeeb234a30ce880
md-emran-hossain <emranhossain4005@gmail.com>
2023-01-04 00:12:15 +0600

    spacing added and unwanted character removed

--- components/Home/Opinion/OpEd/OpEd.js
+++ components/Home/Opinion/OpEd/OpEd.js
@@ -12,11 +12,11 @@ const OpEd = ({ opinion }) => {
       <div className='container'>
         <div className='flex items-center mb-4'>
           <h1 onClick={() => router.push('/opinion')} className='ml-2 text-xl cursor-pointer font-medium text-blue-900'>Opinion </h1><IoIosArrowForward className='text-red-600 mt-1' />
-        </div>?
+        </div>
         <div className="grid grid-cols-12 gap-6">
           <div onClick={() => router.push(`/${oped1?.category}/${oped1?.subCategory}/${oped1._id}`)} style={{ height: "450px" }} className="lg:col-span-5 col-span-12 border-2 border-gray-200 relative flex items-center ml-8 p-12 cursor-pointer text-lg">
             <h1 className='absolute text-xl top-8 bg-blue-900 px-4 -left-6
-             py-2 text-white'>{oped1?.heading}</h1>
+             py-2 text-white mr-6'>{oped1?.heading}</h1>
             <p className='text-gray-600 mt-4'>{oped1?.description[0]?.slice(0, 400)}</p>
             <h2 className='absolute bottom-5 left-4 text-xl text-white'>{oped1?.reporter}</h2>
           </div>
